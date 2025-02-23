37208de866446066fa9f70f07001cc4a597f172c
Will James <35674071+willjames05@users.noreply.github.com>
2025-02-22 21:13:26 -0500

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,20 @@
+let emailId = document.getElementById("email");
+emailId.addEventListener("click", function() {
+    let email = emailId.textContent;
+navigator.clipboard.writeText(email);
+    alert("Copied to clipboard!")
+});
+
+let phoneId = document.getElementById("phoneNumber");
+phoneId.addEventListener("click", function() {
+    let phone = phoneId.textContent;
+navigator.clipboard.writeText(phone);
+    alert("Copied to clipboard!")
+});
+
+let linkedInId = document.getElementById("linkedIn");
+linkedInId.addEventListener("click", function() {
+    let linkedIn = linkedInId.textContent;
+navigator.clipboard.writeText(linkedIn);
+    alert("Copied to clipboard!")
+});
